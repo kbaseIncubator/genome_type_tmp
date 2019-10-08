ac2e6de0c6a8f986dd9e2eb97d729bbc3d2d7b69
Jay R Bolton <jayrbolton@gmail.com>
2019-10-07 17:49:14 -0700

    Get it working with a dry run, and save the resulting json (dryrun_result.json)

--- register_typespec.py
+++ register_typespec.py
@@ -9,14 +9,7 @@ import requests
 
 _WS_URL = "https://ci.kbase.us/services/ws"
 _TOK = os.environ['WS_TOK']
-_SPEC_URL = "https://raw.githubusercontent.com/kbaseIncubator/genome_type_tmp/c38b449c115060f28fa8422d6f410bb1657bd1a4/genome.spec"  # noqa
-
-
-# In [1]: from biokbase.workspace.client import Workspace
-# In [2]: ws = Workspace('https://ci.kbase.us/services/ws')
-# In [3]: m = ws.get_module_info({'mod': 'KBaseGenomes'})
-# In [4]: m.keys()
-# Out[4]: dict_keys(['owners', 'ver', 'spec', 'description', 'types', 'included_spec_version', 'chsum', 'functions', 'is_released'])
+_SPEC_PATH = "./kbase_genomes_module.spec"
 
 
 def get_spec():
@@ -38,12 +31,8 @@ def get_spec():
 
 
 def main():
-    spec = get_spec()
-    print(spec)
-    return
-    genome_spec = requests.get(_SPEC_URL).text
-    # Wrap the spec in the module name
-    spec = "module KBaseGenomes {\n" + genome_spec + "\n}\n"
+    with open(_SPEC_PATH) as fd:
+        spec = fd.read()
     # Make the workspace server method call
     resp = requests.post(
         _WS_URL,
@@ -53,18 +42,15 @@ def main():
             "params": [{
                 "spec": spec,
                 "dryrun": 1,
-                # "dependencies": {
-                #     # TODO this does not work
-                #     "KBaseGenomes": "Feature-7.0"
-                # }
             }],
         })
     )
     resp_json = resp.json()
-    if resp_json['error']:
+    if resp_json.get('error'):
         sys.stderr.write(resp_json['error']['error'] + "\n")
     else:
-        print(resp.text)
+        for (key, val) in resp_json['result'][0].items():
+            print(val)
 
 
 if __name__ == '__main__':
